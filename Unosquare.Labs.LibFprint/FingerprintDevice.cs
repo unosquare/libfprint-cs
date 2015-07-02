2bddbd7d85afb81014da03d62f03f36fff694854
Mario Di Vece <mario@unosquare.com>
2015-07-02 08:09:25 -0500

    adding PGM reader utility

--- Unosquare.Labs.LibFprint/FingerprintDevice.cs
+++ Unosquare.Labs.LibFprint/FingerprintDevice.cs
@@ -62,6 +62,12 @@ namespace Unosquare.Labs.LibFprint
                 {
                     Interop.fp_img_save_to_file(printImagePtr, "last_enrolled.pgm");
                     Interop.fp_img_free(printImagePtr);
+
+                    using (var bitmap = PgmFormatReader.Read("last_enrolled.pgm"))
+                    {
+                        bitmap.Save("last_enrolled.png", System.Drawing.Imaging.ImageFormat.Png);
+                    }
+
                     Console.WriteLine("Image saved :)");
                 }
 
@@ -71,27 +77,28 @@ namespace Unosquare.Labs.LibFprint
 
             // We now have access to the Print Data structure
             var printData = printDataPtr.DereferencePtr<Interop.fp_print_data>();
-            Console.WriteLine ("We have new print data! Type: {0}, Driver: {1}", printData.type, printData.driver_id);
+            Console.WriteLine("We have new print data! Type: {0}, Driver: {1}", printData.type, printData.driver_id);
 
             // We will now need to convert print data to a standard, raw buffer.
             var bufferPtr = IntPtr.Zero;
             var bufferLength = System.Convert.ToInt32(Interop.fp_print_data_get_data(printDataPtr, out bufferPtr));
-            var bufferDataArray = new byte[System.Convert.ToInt32 (bufferLength)];
-            System.Runtime.InteropServices.Marshal.Copy (bufferPtr, bufferDataArray, 0, bufferDataArray.Length);
+            var bufferDataArray = new byte[System.Convert.ToInt32(bufferLength)];
+            System.Runtime.InteropServices.Marshal.Copy(bufferPtr, bufferDataArray, 0, bufferDataArray.Length);
             var firstCharCount = 3;
             var firstChars = new byte[firstCharCount];
-            for (var i = 0; i < firstCharCount; i++) {
-                firstChars[i] = bufferDataArray [i];
+            for (var i = 0; i < firstCharCount; i++)
+            {
+                firstChars[i] = bufferDataArray[i];
             }
 
-            Console.WriteLine ("The standard data buffer has {0} bytes and Reads as follows: {1} (...) ", bufferDataArray.Length, System.Text.Encoding.ASCII.GetString (firstChars));
-            System.IO.File.WriteAllBytes ("sample.print", bufferDataArray);
-            Console.WriteLine ("Raw print data was saved");
+            Console.WriteLine("The standard data buffer has {0} bytes and Reads as follows: {1} (...) ", bufferDataArray.Length, System.Text.Encoding.ASCII.GetString(firstChars));
+            System.IO.File.WriteAllBytes("sample.print", bufferDataArray);
+            Console.WriteLine("Raw print data was saved");
 
             // Now let's test how loading of the raw data bufer works
             var printDataFromBufferPtr = Interop.fp_print_data_from_data(bufferDataArray, System.Convert.ToUInt32(bufferDataArray.Length));
-            var printDataFromBuffer = printDataFromBufferPtr.DereferencePtr<Interop.fp_print_data> ();
-            Console.WriteLine ("We have print data from Buffer -- Type: {0}, Driver: {1}", printDataFromBuffer.type, printDataFromBuffer.driver_id);
+            var printDataFromBuffer = printDataFromBufferPtr.DereferencePtr<Interop.fp_print_data>();
+            Console.WriteLine("We have print data from Buffer -- Type: {0}, Driver: {1}", printDataFromBuffer.type, printDataFromBuffer.driver_id);
 
             // We are the muscal genius of this generation at this point
             // Now, let's create gallery with a print and try to identify it (not very challenging, or is it???)
@@ -99,27 +106,29 @@ namespace Unosquare.Labs.LibFprint
             //     NULL-terminated array of pointers to the prints to identify against. 
             //     Each one must have been previously enrolled with a device compatible to the device selected to perform the scan
 
-            var printGalleryPtrList = new System.Collections.Generic.List<IntPtr> ();
-            printGalleryPtrList.Add (printDataFromBufferPtr);
-            var printGalleryArray = printGalleryPtrList.ToArray ();
+            var printGalleryPtrList = new System.Collections.Generic.List<IntPtr>();
+            printGalleryPtrList.Add(printDataFromBufferPtr);
+            var printGalleryArray = printGalleryPtrList.ToArray();
 
-            Console.WriteLine ("Place your finger on the scanner again.");
+            Console.WriteLine("Place your finger on the scanner again.");
             uint matchOffset = 9999;
-            var matchResult = Interop.fp_identify_finger_img (this.RealDevicePtr, printGalleryArray, ref matchOffset, IntPtr.Zero);
+            var matchResult = Interop.fp_identify_finger_img(this.RealDevicePtr, printGalleryArray, ref matchOffset, IntPtr.Zero);
 
-            if (matchResult >= 0) {
-                Console.WriteLine ("Verify result: " + ((Interop.fp_verify_result)matchResult).ToString());
-                Console.WriteLine ("Match offset resulted in: " + matchOffset);
+            if (matchResult >= 0)
+            {
+                Console.WriteLine("Verify result: " + ((Interop.fp_verify_result)matchResult).ToString());
+                Console.WriteLine("Match offset resulted in: " + matchOffset);
             }
 
             // -- We need the free method to clear the bufferPtr allocated by the lib previously...
             Interop.fp_print_data_free(printDataPtr);
 
-            foreach (var galleryPrintPtr in printGalleryPtrList) {
+            foreach (var galleryPrintPtr in printGalleryPtrList)
+            {
                 if (galleryPrintPtr == IntPtr.Zero)
                     continue;
                 else
-                    System.Runtime.InteropServices.Marshal.FreeHGlobal (galleryPrintPtr);
+                    System.Runtime.InteropServices.Marshal.FreeHGlobal(galleryPrintPtr);
             }
 
 
---
+++ Unosquare.Labs.LibFprint/PgmFormatReader.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Unosquare.Labs.LibFprint
+{
+    public static class PgmFormatReader
+    {
+
+        private static readonly ColorPalette GrayscalePalette = null;
+
+        static PgmFormatReader()
+        {
+            // We initialize the palette to 256 colors (grayscale)
+            // PGM files are always grayscale and we will always represent them in 
+            // 256 shades of gray.
+            using (var dummyBitmap = new Bitmap(1, 1, PixelFormat.Format8bppIndexed))
+            {
+                GrayscalePalette = dummyBitmap.Palette;
+                for (int i = 0; i < 256; i++)
+                {
+                    GrayscalePalette.Entries[i] = Color.FromArgb(i, i, i);
+                }
+            }
+        }
+
+        public static Bitmap Read(Stream stream)
+        {
+            using (var reader = new BinaryReader(stream, Encoding.ASCII))
+            {
+                if ((reader.ReadChar() == 'P' && reader.ReadChar() == '5') == false)
+                    throw new BadImageFormatException("PGM Magic Number not found.");
+
+                // Read onew whitespace character
+                reader.ReadChar();
+
+                // Get basic PGM format properties
+                var parseBuffer = new StringBuilder();
+                var pgmWidth = ReadInteger(reader, parseBuffer);
+                var pgmHeight = ReadInteger(reader, parseBuffer);
+                var grayscaleLevels = ReadInteger(reader, parseBuffer);
+                var isTwoByteLevel = (grayscaleLevels > 255);
+
+                // Create the bitmap and assign the 8bpp Indexed palette
+                var bmp = new Bitmap(pgmWidth, pgmHeight, PixelFormat.Format8bppIndexed);
+                bmp.Palette = GrayscalePalette;
+
+                // Create the target lockbits
+                var bitmapData = bmp.LockBits(new Rectangle(0, 0, pgmWidth, pgmHeight), ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
+                var rowLengthOffset = bitmapData.Stride - bitmapData.Width;
+                var dataLength = bitmapData.Stride * bitmapData.Height;
+                var pixelData = new byte[dataLength];
+
+                var pixelDataIndex = 0;
+                var currentPixelValue = (byte)0;
+
+                for (int rowIndex = 0; rowIndex < pgmHeight; rowIndex++)
+                {
+                    for (int columnIndex = 0; columnIndex < pgmWidth; columnIndex++)
+                    {
+                        currentPixelValue = reader.ReadByte();
+                        if (isTwoByteLevel)
+                            currentPixelValue = (byte)(((double)((currentPixelValue << 8) + reader.ReadByte()) / grayscaleLevels) * 255.0);
+
+                        pixelData[pixelDataIndex] = currentPixelValue;
+                        pixelDataIndex++;
+                    }
+
+                    pixelDataIndex += rowLengthOffset;
+                }
+
+                // Use a fast copy mechanism to write the data to the bitmap
+                System.Runtime.InteropServices.Marshal.Copy(pixelData, 0, bitmapData.Scan0, dataLength);
+
+                // Release the write lock on the bits.
+                bmp.UnlockBits(bitmapData);
+
+                return bmp;
+            }
+        }
+
+        public static Bitmap Read(byte[] buffer)
+        {
+            using (var ms = new MemoryStream(buffer))
+            {
+                ms.Position = 0;
+                return Read(ms);
+            }
+        }
+
+        public static Bitmap Read(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                return Read(fs);
+            }
+        }
+
+        private static int ReadInteger(BinaryReader reader, StringBuilder sb)
+        {
+            var c = '\0';
+            sb.Length = 0;
+            while (char.IsDigit(c = reader.ReadChar()))
+            {
+                sb.Append(c);
+            }
+
+            return int.Parse(sb.ToString().Trim());
+        }
+    }
+}
